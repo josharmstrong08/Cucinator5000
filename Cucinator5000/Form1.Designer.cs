7f60df3da76a6dc18b1e480b750dd90184bdb43a
Josh Armstrong <josh.armstrong@salesforce.com>
2019-09-14 07:42:35 -0400

    Initial commit

---
+++ Cucinator5000/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Cucinator5000
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cuceSilver = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cuceSilver)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cuceSilver
+            // 
+            this.cuceSilver.Image = global::Cucinator5000.Properties.Resources.cuce;
+            this.cuceSilver.Location = new System.Drawing.Point(190, 136);
+            this.cuceSilver.Name = "cuceSilver";
+            this.cuceSilver.Size = new System.Drawing.Size(231, 114);
+            this.cuceSilver.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cuceSilver.TabIndex = 0;
+            this.cuceSilver.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Use Arrow Keys";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cuceSilver);
+            this.Name = "Form1";
+            this.Text = "Cucinator 5000";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.cuceSilver)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cuceSilver;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Cucinator5000/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cucinator5000
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int step = 10;
+            switch (e.KeyCode)
+            {
+                case Keys.Down:
+                    cuceSilver.Top += step;
+                    break;
+                case Keys.Up:
+                    cuceSilver.Top -= step;
+                    break;
+                case Keys.Left:
+                    cuceSilver.Left -= step;
+                    break;
+                case Keys.Right:
+                    cuceSilver.Left += step;
+                    break;
+            }
+        }
+    }
+}
